3bb3fbfdc06853f889a6dfa1ff4f2fbea8b3c9ae
Kirthy Rajan <130735790+KirthyRajan02@users.noreply.github.com>
2023-08-24 22:02:43 +0530

    Add files via upload

---
+++ quiz/quiz.js
@@ -0,0 +1,136 @@
+
+var startTime;
+
+var questionBank= [
+    
+    {
+        question : ' Software project manager is engaged with software management activities. He is responsible for',
+        option : ['Project planning','Monitoring the process','Communication among stake holders','All mentioned above'],
+        answer : 'All mentioned above'
+    },
+    {
+        question : ' Which of the following is not a characteristic of a software project',
+        option : ['Junagarh, Gujarat','Diphu, Assam','Kohima, Nagaland','Gangtok, Sikkim'],
+        answer : 'Diphu, Assam'
+    },
+    {
+        question : 'For which of the following disciplines is Nobel Prize awarded?',
+        option : ['Physics and Chemistry','Physiology or Medicine','Literature, Peace and Economics','All of the above'],
+        answer : 'All of the above'
+    },
+    {
+        question : 'Hitler party which came into power in 1933 is known as',
+        option : ['Labour Party','Nazi Party','Ku-Klux-Klan','Democratic Party'],
+        answer : 'Nazi Party'
+    }
+]
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var score= 0;
+
+//function to display questions
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+}
+
+//function to calculate scores
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'green';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'red';
+    }
+   setTimeout(nextQuestion,1000);
+}
+
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+        
+      
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+//function to check Answers
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+
+
+displayQuestion();
+
+            var minutes = 40;
+            var seconds = 0;
+
+// Calculate the total number of seconds
+var totalSeconds = minutes * 60 + seconds;
+
+// Get the timer element
+var timerElement = document.getElementById("timer");
+
+// Update the timer every second
+var timer = setInterval(function() {
+  // Calculate the minutes and seconds
+  minutes = Math.floor(totalSeconds / 60);
+  seconds = totalSeconds % 60;
+
+  // Display the timer
+  timerElement.innerHTML = "<br><b>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;TIME LEFT: " + minutes + "m " + seconds + "s"+"<br><br>";
+
+  // Check if the time has run out
+  if (totalSeconds <= 0) {
+    clearInterval(timer);
+    
+    alert("Time's up!");
+  }
+
+  // Decrement the totalSeconds
+  totalSeconds--;
+}, 800);
